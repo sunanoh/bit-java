e7928597ed310946d5b403d9c4b7cf3528efce0f
bonanza <bonanza1993@naver.com>
2020-08-04 11:15:58 +0900

    1번째 commit

---
+++ javaEx/src/javaEx/com/basic/HelloWorld.java
@@ -0,0 +1,12 @@
+package javaEx.com.basic;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello world");
+		
+	}
+
+}
