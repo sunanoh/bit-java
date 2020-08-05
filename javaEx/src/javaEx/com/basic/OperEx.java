6f0379c95f764f027ebad8757dbf2b732b146650
Sukyeong Noh <bonanza1993@naver.com>
2020-08-05 12:48:10 +0900

    산술연산자 포함

---
+++ javaEx/src/javaEx/com/basic/OperEx.java
@@ -0,0 +1,30 @@
+package javaEx.com.basic;
+
+public class OperEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		arithOper();
+	}
+
+	
+	// 산술연산자
+	public static void arithOper() {
+		// 사칙연산 : +, -, , /
+		int a = 7;
+		int b = 3;
+		
+		System.out.println("7 / 3 = " + (7/3));
+		// int / int -> int(몫)
+		System.out.println("7 / 3의 몫 = " + (7%3));
+		// 나머지연산자 : %
+		
+		// 정확한 실수값을 얻으려면 int -> float 변환
+		
+		System.out.println("7 / 3 실수 => " + (float)a/(float)b);
+		
+		
+		// 만약, 정수를 0으로 나누면
+		//System.out.println("정수 나누기 0 " + (a/0));
+	}
+}
